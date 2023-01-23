5d37b2f8d8a851a17694dc94f558d68e906532dd
Mihir Patel <65970068+mihir09@users.noreply.github.com>
2023-01-23 13:46:01 -0800

    Update settings.py

--- letschatproject/settings.py
+++ letschatproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-be5rjodin8no255(gq(y%lanry$#_0&knvb6p90pgkblhsacra
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
