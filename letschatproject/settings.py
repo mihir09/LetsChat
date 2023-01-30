82ccc14409c0eb0841c46d5c19b3b77fa0b257c8
Mihir Patel <65970068+mihir09@users.noreply.github.com>
2023-01-30 12:23:08 -0800

    production changed

--- letschatproject/settings.py
+++ letschatproject/settings.py
@@ -35,7 +35,7 @@ ROOT_URLCONF = f'{config("PROJECT_NAME")}.urls'
 
 WSGI_APPLICATION = f'{config("PROJECT_NAME")}.wsgi.application'
 
-ASGI_APPLICATION = f'{config("PROJECT_NAME")}.room.routing.application'
+ASGI_APPLICATION = f'routing.application'
 
 
 ALLOWED_HOSTS += ['localhost', '127.0.0.1', '.ngrok.io']
