830788dbf11760da04b8a5a185680ee6c0489d4f
Mihir Patel <65970068+mihir09@users.noreply.github.com>
2023-01-30 18:57:29 -0800

    changed routing file

--- room/routing.py
+++ room/routing.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/<str:room_name>/', consumers.ChatConsumer),
+    path('ws/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
 ]
